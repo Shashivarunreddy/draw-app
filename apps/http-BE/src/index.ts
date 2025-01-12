65a89261240e2c3e21ba2c5f87cdf1e115a3ff45
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-01-13 00:38:24 +0530

    added zod in global packages

--- apps/http-BE/src/index.ts
+++ apps/http-BE/src/index.ts
@@ -1,22 +1,33 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import {JWT_SECRET} from "@repo/backend-common/config";
 import { middleware } from './middleware';
+import { CreateUserSchema , SigninSchema , CreateRoonSchema } from '@repo/common/types';
 
 const app = express();
 
 app.post("/signup", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
 
+ const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message: "Invalid data"
+    })
+    return;
+  }
   res.json({
     roomId: "123"
   })
 })
 
 app.post("/signin", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const data = SigninSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message: "Invalid data"
+    })
+    return;
+  }
 
 
   const userId = 1;
@@ -29,7 +40,13 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room",middleware,  (req, res) => {
-
+  const data = CreateRoonSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message: "Invalid data"
+    })
+    return;
+  }
 
   res.json({
     roomId: 123
