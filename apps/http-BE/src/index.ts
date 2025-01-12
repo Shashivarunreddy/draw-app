6f7cf305094717915c4f6fd7f50a433837db25ef
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-01-12 23:34:28 +0530

    added sigmup signin room

--- apps/http-BE/src/index.ts
+++ apps/http-BE/src/index.ts
@@ -1,9 +1,40 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+import { middleware } from './middleware';
 
 const app = express();
 
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  res.json({
+    roomId: "123"
+  })
+})
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
 
+  const userId = 1;
+  const token = jwt.sign({
+    userId
+  }, JWT_SECRET)
+  res.json({
+    token
+  })
+})
+
+app.post("/room",middleware,  (req, res) => {
+
+
+  res.json({
+    roomId: 123
+  })
+})
 
 
 app.listen(3000);
