e587a886094b835e5d5bdebf3e40f97b250e4546
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-03-05 06:56:38 +0530

    added signup end point

--- apps/http-BE/src/index.ts
+++ apps/http-BE/src/index.ts
@@ -4,21 +4,37 @@ import jwt from 'jsonwebtoken';
 import { middleware } from './middleware';
 import { CreateUserSchema , SigninSchema , CreateRoonSchema } from '@repo/common/types';
 import { JWT_SECRET } from '@repo/backend-common/config';
+import { prismaClient } from '@repo/db/client';
+
 
 const app = express();
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
 
- const data = CreateUserSchema.safeParse(req.body);
-  if(!data.success){
+ const parseData = CreateUserSchema.safeParse(req.body);
+  if(!parseData.success){
     res.json({
       message: "Invalid data"
     })
     return;
   }
-  res.json({
-    roomId: "123"
-  })
+  try{
+    await prismaClient.user.create({
+      data: {
+        email: parseData.data?.username,
+        password: parseData.data?.password,
+        name: parseData.data.name
+      }
+  
+    })
+    res.json({
+      roomId: "123"
+    })
+  } catch(e){
+    res.status(411).json({
+      message: "User already exists with this email"
+    })
+  }
 })
 
 app.post("/signin", (req, res) => {
